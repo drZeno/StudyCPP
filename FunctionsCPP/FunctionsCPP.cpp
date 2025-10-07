b817b522de7b418042ef20f0786b66eaa87bfc52
drZeno <zenoamadeus1400@proton.me>
2025-10-07 19:18:06 +0300

    Functions how work

--- FunctionsCPP/FunctionsCPP.cpp
+++ FunctionsCPP/FunctionsCPP.cpp
@@ -2,6 +2,7 @@
 using namespace std;
 
 // Определяем функцию **вне main**
+
 int Plusfunction(int number_one, int number_two)
 {
     int local_Variable = number_one + number_two;
@@ -12,5 +13,8 @@ int Plusfunction(int number_one, int number_two)
 int main()
 {
     int sumhowmuch = Plusfunction(1, 2);
-    return 0;
+    int anothervar = Plusfunction(sumhowmuch, 2);
+    cout << Plusfunction(sumhowmuch, anothervar) << endl; 
+    
+    
 }
