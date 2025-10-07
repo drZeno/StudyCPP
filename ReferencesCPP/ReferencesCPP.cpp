1ad627a715b9907049a413c2447c4dc2fe126147
drZeno <zenoamadeus1400@proton.me>
2025-10-08 01:56:29 +0300

    Something about ReferencesCPP

---
+++ ReferencesCPP/ReferencesCPP.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+
+void ChangeStr(string& str)
+{
+    str += "!";
+}
+
+int main()
+{
+    string myStr = "Amadeus";
+    ChangeStr(myStr);
+
+    cout << myStr << endl;
+    
+    system("pause");
+}
+
