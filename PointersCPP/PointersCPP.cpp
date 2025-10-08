1a289b3c981a8c58584baea614c3e4fb0b8b1d3b
drZeno <zenoamadeus1400@proton.me>
2025-10-08 19:43:33 +0300

    MoreAboutPointers

--- PointersCPP/PointersCPP.cpp
+++ PointersCPP/PointersCPP.cpp
@@ -2,14 +2,43 @@
 #include <string>
 using namespace std;
 
-int justNumber = 13;
+int a = 100;
+int b = 50;
+
+struct Container
+{
+    string name;
+    int x;
+    int y;
+    int z;
+};
+
+int justnumber = 0;
 
 int main ()
 {
-    int* justNumberPointer = &justNumber;
+    Container container = {"Amadeus",1,2,3};
+    Container* PtrToCont = &container;
+    //СКОБКИ НУЖНЫ ЧТОБЫ СКАЗАТЬ КОМПИЛЯТОРУ ЧТО ДЕЛАТЬ ПЕРВЫМ.
+    //БЕЗ СКОБОК ОН БУДЕТ СМОТРЕТЬ СПРАВА НА ЛЕВО, А БЕЗ НИХ ОН НЕ ОПРЕДЕЛЕН.
+    cout << (*PtrToCont).x << endl;
+    
+    //БОЛЕЕ УДОБНЫЙ СПОСОБ
+    cout << PtrToCont->x << endl;
+
+    justnumber += PtrToCont->z;
+    cout << "Just number: " << justnumber << endl;
+    
+    int* justNumberPointer = &a;
     cout << *justNumberPointer << endl;
     
      *justNumberPointer += 2;
     cout << *justNumberPointer << endl;
-    cout << justNumber << endl;
+    cout << a << endl;
+    int numbers[] = { 1, 2, 3, 4, 5 };
+    int* NumPtr = numbers;
+    cout << *NumPtr << endl;
+    NumPtr++;
+    cout << *NumPtr << endl;
+    
 }
