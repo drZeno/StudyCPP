6fe0527f853301a68e59be6df7790605c562db95
drZeno <zenoamadeus1400@proton.me>
2025-10-09 04:37:17 +0300

    Push

--- ConstructorsCPP/ConstructorsCPP.cpp
+++ ConstructorsCPP/ConstructorsCPP.cpp
@@ -4,26 +4,32 @@ using namespace std;
 
 struct Cat
 {
-    Cat();
 
+    Cat();
     int age;
     float weight;
     string name;
     
+    Cat(const string& n="Unnamed", int a=0, float w=0.0f)
+      : age(a), weight(w), name(n) 
+    {
+        cout << "Cat constructor !" << endl;
+        Meow();
+    }
+
     void Meow();
-    
 };
+
 
 void Cat::Meow()
 {
     cout << "Meow constructor ! My age is " << age << ".\n";
     cout << "Meow weight is ! " << weight << ".\n";
-    cout << "HELLO ! " << ".\n";
+    cout << name << ".\n";
 };
 
 Cat::Cat()
 {
-    cout << "Cat constructor !" << endl;
     age = 1;
     weight = 2.5;
     name = "HelloCat";
